777fec086e39009c3881358c08d46425a2316e49
M.Charattrakool <m.charattrakool@gmail.com>
2016-11-11 01:29:32 +0700

    add left hand checker logic

--- core/src/com/mygdx/game/GameRenderer.java
+++ core/src/com/mygdx/game/GameRenderer.java
@@ -10,6 +10,7 @@ public class GameRenderer {
 
 
 	private Texture[] squareImage;
+	private int[][] boxLocation;
 
 	
 	public GameRenderer(TiberSeptim tiberSeptim,GameLogicCalculator gameLogicCalculator){
@@ -20,13 +21,19 @@ public class GameRenderer {
 		squareImage[0] = new Texture("img/sq-yellow.png");
 		squareImage[1] = new Texture("img/sq-blue.png");
 
+		boxLocation = new int[][]{
+			{400,400},
+			{300,300},
+			{400,200},
+			{500,300}
+		};
 		
 	}
 	
 	public void render(float delta){
         SpriteBatch batch = tiberSeptim.batch;
         batch.begin();
-        batch.draw(squareImage[0], 100, 50 ,25 ,25);
+        for(int i=0;i<4;i++)batch.draw(squareImage[0], boxLocation[i][0],boxLocation[i][1] ,25 ,25);
         batch.end();	
 	}
 	
